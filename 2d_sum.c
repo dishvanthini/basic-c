b588449029110c376e11255a2bca114688cb5fc6
DISHVANTHINI <150140881+dishvanthini@users.noreply.github.com>
2024-08-29 18:49:08 +0530

    Create 2d_sum.c

---
+++ 2d_sum.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n, m,i,j;
+    scanf("%d %d", &n, &m);
+
+    int a[n][m], b[n][m];
+    
+    for (i = 0; i < n; i++) {
+        for ( j = 0; j < m; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for ( i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    for ( i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            printf("%d ", a[i][j] + b[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
